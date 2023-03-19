4d573dfc495a1f3af065b139ee211fb012cb2c94
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2023-03-19 15:48:38 +0200

    Add libraries

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let intervalId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+startButton.addEventListener('click', () => {
+    if(intervalId) return;
+    intervalId = setInterval(changeBackgroundColor, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+    intervalId = clearInterval(intervalId);
+});
+
 
